5adf6d8607dc77a0bfb0443fa0c36ac2ee567a91
Nandhini-Ramanathan <111539321+Nandhini-Ramanathan@users.noreply.github.com>
2023-05-12 12:51:09 -0600

    Add files via upload

---
+++ src/Main/MAINNN/ninja.py
@@ -0,0 +1,15 @@
+#Nandhini
+class Ninja(object):
+    def __init__(self, xpos, ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.h = 20
+
+    def display(self):
+        # fill(0)
+        # noStroke()
+        # rect(self.xpos, self.ypos, 30, 50)
+        img = loadImage("running.png")
+        image(img, self.xpos, self.ypos)
+        
+        
