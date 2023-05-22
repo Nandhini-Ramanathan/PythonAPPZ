c244f6d1f8ebe6ad87d2d6be15b2f87cd39ece2d
Jack-Bark <111711527+Jack-Bark@users.noreply.github.com>
2023-05-21 21:45:13 -0600

    Delete src/Main directory

--- src/Main/Main Updated 5.10.23/button.py
+++
@@ -1,9 +0,0 @@
-class Button(object):
-    
-    def __init__(self, xpos, ypos):
-        self.xpos = xpos
-        self.xpos = ypos
-        
-    def display(self):
-        rectMode(CENTER)
-        rect(self.xpos, self.ypos, 50, 30)
